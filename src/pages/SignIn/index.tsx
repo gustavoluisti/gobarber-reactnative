a51bf5c8378871d423385ff4e49dfbabdae0b77d
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-04-29 19:19:51 -0300

    Criando rotas iniciais

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './style';
+
+const Signin: React.FC = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+export default Signin;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './style';
+
+const SignUp: React.FC = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+export default SignUp;
